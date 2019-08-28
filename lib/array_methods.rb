77b33b00e3382c3c7adb3d29a2ace762433f925a
Laura Berge <lauraeberge@gmail.com>
2019-08-28 01:51:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,12 @@
 def find_element_index(array, value_to_find)
-
+  index = 0
+  while array[index] do
+    if array[index] == value_to_find {
+      return index
+    } else {
+      index += 1
+    }
+  end
 end
 
 def find_max_value(array)
