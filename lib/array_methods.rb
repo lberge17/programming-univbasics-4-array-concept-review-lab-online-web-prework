cc09ea0ee7935df55104e545b0d2056a062c5c7a
Laura Berge <lauraeberge@gmail.com>
2019-08-28 01:52:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  max_num_array = array.sort()
-  max_num = max_num_array[-1]
+  num_array = array.sort()
+  max_num = num_array[-1]
   return max_num
 end
 
 def find_min_value(array)
-  min_val_array
+  num_array = array.sort()
+  min_num = num_array[0]
+  return min_num
 end
